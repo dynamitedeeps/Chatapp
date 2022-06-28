b819ad5ae99d49a2968033ba53b7d6da7f366b60
dynamitedeeps <sonkhladeepanshu@gmail.com>
2022-06-28 16:15:31 +0530

    With vue updates

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/chat',function(){
+	return view('chat');
+});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
